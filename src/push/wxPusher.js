4d1cc2873f7ea299b4d6ad6cb3882341c7799e94
Azure <136671891@qq.com>
2024-10-29 08:54:26 +0800

    Update wxPusher.js

--- src/push/wxPusher.js
+++ src/push/wxPusher.js
@@ -1,5 +1,5 @@
 module.exports = {
   appToken:
-    process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
+    process.env.WX_PUSHER_APP_TOKEN || "AT_bAsE8tpbTZ48F6R1Sgkda5SLY0Uss8GP",
   uid: process.env.WX_PUSHER_UID || "",
 };
